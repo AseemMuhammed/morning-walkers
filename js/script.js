92bd1ad271108f9e819889a37621d89a77ea882e
hanan <hananpilakkadan@gmail.com>
2022-11-19 14:42:43 +0530

    updated-pages

--- js/script.js
+++ js/script.js
@@ -54,6 +54,43 @@ $(".events-slider").slick({
     },
   ],
 });
+$(".coment-section").slick({
+  dots: false,
+  infinite: true,
+  speed: 2000,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2500,
+  arrows: true,
+  prevArrow: "<i class='fa fa-angle-left' aria-hidden='true'></i>",
+  nextArrow: "<i class='fa-solid fa-angle-right'></i>",
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
 
 // navlinks-activating-whiling-scrollig
 const sections = document.querySelectorAll("section");
